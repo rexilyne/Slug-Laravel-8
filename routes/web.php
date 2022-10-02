3f68bcc4add22676f33fd36a09151d4193248a03
Adrian Metanoia Gawa <tfadriangawa@gmail.com>
2022-10-02 23:52:56 +0700

    add post route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(PostController::class)->group(function() {
+    Route::post('/post', 'store');
+    Route::get('/post', 'store');
+});
