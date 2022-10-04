038e55237251d0f8f72caee7d9b380864a68e5a8
Adrian Metanoia Gawa <tfadriangawa@gmail.com>
2022-10-04 23:53:43 +0700

    update post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
 
+    protected $guarded = [];
 
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
