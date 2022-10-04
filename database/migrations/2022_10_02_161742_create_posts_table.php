edbc47f195d3f75fb740d5c10137ca73df5d5cf7
Adrian Metanoia Gawa <tfadriangawa@gmail.com>
2022-10-05 00:01:17 +0700

    udpate migration

--- database/migrations/2022_10_02_161742_create_posts_table.php
+++ database/migrations/2022_10_02_161742_create_posts_table.php
@@ -16,8 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug');
-            $table->text('body');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
